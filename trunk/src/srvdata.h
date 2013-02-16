2cbde8da3ee5f61458c4cebbfbe73c428750ca52
suleman1971@gmail.com <suleman1971@gmail.com@fd42d2be-1902-1130-0d52-23a802b86b00>
2013-02-16 16:56:54 +0000

    - back to the quilt format (debian/rules changed because "dpkg-buildpackage -F" crash on quilt format) - description headers added to all source files - DEB_BUILD_HARDENDING=1 set in the debian/rules file - removed /debian/lintian-overrides file

--- trunk/src/srvdata.h
+++ trunk/src/srvdata.h
@@ -1,3 +1,20 @@
+// =============================================================================
+// This file is part of boinctui.
+// http://boinctui.googlecode.com
+// Copyright (C) 2012,2013 Sergey Suslov
+//
+// boinctui is free software; you can redistribute it and/or modify it  under
+// the terms of the GNU Lesser General Public License as published by the
+// Free Software Foundation, either version 3 of the License, or (at your
+// option) any later version.
+//
+// boinctui is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU Lesser General Public License for more details
+// <http://www.gnu.org/licenses/>.
+// =============================================================================
+
 #ifndef SRVDATA_H
 #define SRVDATA_H
 
