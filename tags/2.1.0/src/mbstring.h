22b4ae94a68766574a688ac633511f8528d802d9
suleman1971@gmail.com <suleman1971@gmail.com@fd42d2be-1902-1130-0d52-23a802b86b00>
2013-05-14 07:52:20 +0000

    delete 2.1.0 tag

--- tags/2.1.0/src/mbstring.h
+++
@@ -1,8 +0,0 @@
-#ifndef MBSTRING_H
-#define MBSTRING_H
-
-int mbstrlen(const char* s); //вернет размер строки utf8 в СИМВОЛАХ (не байтах) без учета '\0'
-
-char* mbstrtrunc(char* s, int slen); //обрезать до длины slen символов (не байтов) без учета \0
-
-#endif // MBSTRING_H
